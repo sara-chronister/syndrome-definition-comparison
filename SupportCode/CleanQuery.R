9fe9964b07fe272db35a893dd7c810eb39a99096
Sara Chronister <sara.chronister@gmail.com>
2021-07-26 09:26:24 -0700

    Additional query parsing
    Updates to !term! syntax and added element type for CCDD categories listed in the definition

--- SupportCode/CleanQuery.R
+++ SupportCode/CleanQuery.R
@@ -1,16 +1,20 @@
 
-
 clean_query_essence <- function(query) {
   
   clean_query <- data.frame(Element = query) %>%
     mutate(Element = str_to_lower(Element)) %>%
     mutate(Element = str_replace_all(Element, ",andnot,\\^.*?\\^|,andnot,\\(.*?\\)", "")) %>%
+    mutate(Element = str_replace_all(Element, "!","^")) %>%
     mutate(Element = str_replace_all(Element,",and,",",or,")) %>%
-    cSplit(., splitCols = "Element", sep = ",or,", type.convert = FALSE) %>%
+    mutate(Element = str_replace_all(Element,",or,","|")) %>%
+    cSplit(., splitCols = "Element", sep = "|", type.convert = FALSE) %>%
     pivot_longer(cols = starts_with("Element"), values_to = "Element") %>%
     mutate(Element = str_replace_all(Element,"\\[;/ \\]|\\[;/\\]","")) %>%
     mutate(Element = str_replace_all(Element,"\\)|\\(|\\^|,|;|/|\\.","")) %>%
-    mutate(Type = ifelse(str_detect(Element,"[[:digit:]]"),"Diagnosis code","Syndrome term")) %>%
+    mutate(Type = case_when(
+      str_detect(Element,"v[[:digit:]]") ~ "CCDD Category (see ESSENCE)",
+      str_detect(Element,"[[:digit:]]") ~ "Diagnosis Code",
+      str_detect(Element, "[[:digit:]]", negate = TRUE) ~ "Syndrome Term")) %>%
     select(-name, `Syndrome Element` = Element, `Element Type` = Type) %>%
     dplyr::distinct()
   
